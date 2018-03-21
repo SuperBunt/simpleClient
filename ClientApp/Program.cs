0f73a1c5ac74010050d50f3a5ac121d13f2846fb
peter connell <pec@ersl.ie>
2018-03-21 16:38:32 +0000

    First commit

---
+++ ClientApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaftServiceClient client = new DaftServiceClient();
+
+            client.GeneratePDF("https://stackoverflow.com/questions/20424425/recommended-size-of-icon-for-google-chrome-extension");
+
+            Console.WriteLine("Hello");
+
+            client.Close();
+
+            Console.ReadKey();
+
+        }
+    }
+}
